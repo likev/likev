faca2da8ac555fedda6fdb64414be9c69b51710c
xufanglu <xufanglu@6c959574-b92d-50af-55bd-85a64169e16b>
2011-06-08 05:58:30 +0000

    fixed problem

--- web-design/rain/henan/script/ready.js
+++ web-design/rain/henan/script/ready.js
@@ -96,77 +96,7 @@ function initialize() {
 	
 	lyRain.reloadAllData((new Date()).getTime());
     window.setInterval("lyRain.reloadAllData((new Date()).getTime())",60*1000);
-		
-	$("#alarm-num").val(lyRain.alarmValue);
-	var beginTime = new Date();
-	if(beginTime.getHours()<20) beginTime.setDate(beginTime.getDate()-1);
-	beginTime.setHours(20,0,0);
-	
-	var defaultBegin = lyRain.format_time(beginTime);
-	$("#beginTime").val(defaultBegin);
-	$("#endTime").val(lyRain.format_time());
-	
-	$("#setend").click(function(){
-		if($(this).is(":checked")){
-			$("#endTime").removeAttr("disabled");
-			
-			if(!$("#setbegin").is(":checked")){
-				$("#setbegin")[0].checked = true;
-				$("#beginTime").removeAttr("disabled");
-			}		
-		}
-		else{
-			$("#endTime").attr("disabled","disabled");
-		}
-	});
-	
-	$("#setbegin").click(function(){
-		if($(this).is(":checked")){
-			$("#beginTime").removeAttr("disabled");
-		}
-		else{
-			$("#beginTime").attr("disabled","disabled");
-			
-			if($("#setend").is(":checked")){
-				$("#setend")[0].checked=false;
-				$("#endTime").attr("disabled","disabled");
-			}			
-		}
-	});
-	
-	$("#set-alarm").click(function(){
-	    if(this.checked){
-	        $("#alarm-num").removeAttr("disabled");
-	        lyRain.isSetAlarm = true;
-	    }
-	    else{
-	        $("#alarm-num").attr("disabled","disabled");
-	        lyRain.isSetAlarm = false;
-	    }
-	});
-	
-	$("#collaps").click(function(){
-		$("#map-toolbar").css("width","60%")
-		$("#left").css("width","60%");
-		$("#collaps").hide();
-		$("#expand").show();
-	});
-	$("#expand").click(function(){
-		$("#map-toolbar").css("width","100%");
-		$("#left").css("width","100%");
-		$("#collaps").show();
-		$("#expand").hide();
-	});
-	
-	$("#info-toolbar .header a").click(function(){
-		$("#info-toolbar .header span").hide();
-		$("#info-toolbar .header a").show();
-		$(this).hide();
-		$(this).next("span").show();
-		
-		$(".rain-info").hide();
-		$($(this).attr('mapid')).show();
-	});
+
 	
 	//点击站名时将地图放大并置于中心
 	$("#stat-info").delegate("td.rain-station a","click",function(){
@@ -179,28 +109,10 @@ function initialize() {
 	
 	//检测是否支持报警
 	if(!('play' in $("<audio/>")[0]) ){
-	    $(".tips").show();
-	    $("#set-alarm").attr("disabled","disabled");
+	    $("#alarm-volume-check").attr({checked:false, disabled:"disabled"});
+		$("#alarm-volume-test").text("您的浏览器不支持报警，请切换firefox或chrome浏览器");
 	}
-	
-	$("#alarm-num").change(function(){
-		var newValue = Number(this.value);
-	    //if( newValue > lyRain.alarmValue){
-			lyRain.alarmedStation = [];
-		//}
-		
-		lyRain.alarmValue = newValue;
-		
-		lyRain.dealAlarm(true);
-	}) ;
-	
-	$("#set input").change(function(){
-		$("#ajax-info").text("修改已生效 将重新加载数据");
-		lyRain.removeRain();
-		$(".rain-info").not('#alarm-info').hide();
-		lyRain.reloadRainData((new Date()).getTime());
-	}) ;
-	
+
 	//某些浏览器不支持audio loop
 	$('#alarm-sound').bind('ended',function(){
 		this.play();
