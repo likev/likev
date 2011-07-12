7caf0de1d071755c3e974447532cd9cc78c2957f
xufanglu <xufanglu@6c959574-b92d-50af-55bd-85a64169e16b>
2011-07-12 06:08:23 +0000

    增加上传短时预报

--- MessAtoB/MessAtoB/resource.h
+++ MessAtoB/MessAtoB/resource.h
@@ -11,6 +11,7 @@
 #define IDD_DIALOG1                     130
 #define IDD_DIALOG_PY                   130
 #define IDD_DLG_SelfForcast             131
+#define IDD_DLG_ShortForcast            132
 #define IDC_EDIT1                       1000
 #define ID_CHE                          1001
 #define IDC_BUTTON1                     1002
@@ -39,14 +40,17 @@
 #define IDC_COMBO_Names                 1040
 #define IDC_BUTTON_SelfForcast          1043
 #define IDC_EDIT_FileContent            1044
+#define IDC_BUTTON_SHORTFORCAST         1045
+#define IDC_EDIT_Content                1046
+#define IDC_EDIT_FContent               1046
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1045
+#define _APS_NEXT_CONTROL_VALUE         1047
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
