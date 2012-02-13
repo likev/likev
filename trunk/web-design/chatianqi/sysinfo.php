0a104a1300734660f758ca29357b55d37d42c678
xufanglu <xufanglu@6c959574-b92d-50af-55bd-85a64169e16b>
2012-02-13 03:42:25 +0000

    add system cpu check

---
+++ trunk/web-design/chatianqi/sysinfo.php
@@ -0,0 +1,23 @@
+<?php
+//get free cpu and memeory
+function get_cpufree(){
+	$cmd =  "top -n 1 -b -d 0.1 | grep 'Cpu'";//'free -m'- 
+	$lastline = exec($cmd,$output);
+
+	preg_match('/(\S+)%id/',$lastline, $matches);
+	$cpufree = $matches[1];
+	return $cpufree;
+}
+
+function get_memfree(){
+	$cmd =  'free -m';//"top -n 1 -b -d 0.1 | grep 'Cpu'"
+	$lastline = exec($cmd,$output);
+
+	preg_match('/Mem:\s+(\d+)/',$output[1], $matches);
+	$memtotal = $matches[1];
+	preg_match('/(\d+)$/',$output[2], $matches);
+	$memfree = $matches[1]*100.0/$memtotal;
+	
+	return $memfree;
+}
+?>
