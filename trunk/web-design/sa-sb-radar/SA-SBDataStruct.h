a0db5fb0ed70127250fd5966219c9c10df918dc2
xufanglu <xufanglu@6c959574-b92d-50af-55bd-85a64169e16b>
2012-07-22 16:59:40 +0000

    update SA-SBDataStruct.h

--- trunk/web-design/sa-sb-radar/SA-SBDataStruct.h
+++ trunk/web-design/sa-sb-radar/SA-SBDataStruct.h
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <map>
+#include <sstream>
 
 //DataBase.h
 //SA，SB雷达的距离库数
@@ -23,18 +25,11 @@
 #define WGates  1600			//C BAND 谱宽距离库数
 */
 
-#define MaxCuts		20			//最大仰角层数
-#define MaxRads		360			//每层仰角上的方位数,每度保留一个径向
 
-#define CODE_INVALID 0			//编码值中的特殊标记,表示无有效观测数据
-#define CODE_RANFOLD 1			//编码值中的特殊标记,表示有距离模糊
 
 #define VALUE_INVALID -999.		//实际值中的特殊标记,表示无有效观测数据
 #define VALUE_RANFOLD  999.		//实际值中的特殊标记,表示有距离模糊
 #define VALUE_AINVALID -1;
-
-#define RES_POINT_FIVE 2		//速度精度类型,代表的精度为0.5 M/S
-#define RES_ONE_POINT  4		//速度精度类型,代表的精度为1.0 M/S
 
 #define RADIAN  3.14159/180.
 #define PI 3.14159
@@ -82,10 +77,173 @@ struct RadarBaseData
 
 };
 
+class RadarElevation{
+public:
+	enum CODE_MAEK{
+		CODE_INVALID = 0,			//编码值中的特殊标记,表示无有效观测数据
+		CODE_RANFOLD = 1          //编码值中的特殊标记,表示有距离模糊
+	};
+
+	enum VELOCITY_RESOLUTION {
+		RES_POINT_FIVE = 2,		//速度精度类型,代表的精度为0.5 M/S
+		RES_ONE_POINT = 4		//速度精度类型,代表的精度为1.0 M/S
+	};
+public:
+	std::vector<std::vector<double> > r, v, w;
+
+	bool r_valid, v_valid, w_valid, 
+		r_valid_this, v_valid_this, w_valid_this;
+
+	unsigned r_pos, v_pos, w_pos;
+
+	double Elevation;
+
+	unsigned int r_gate_length, vw_gate_length, 
+		r_gate_count, vw_gate_count; 
+
+	double r_first_distance, vw_first_distance;
+
+	int v_resolution;
+public:
+	RadarElevation():
+		r_valid(false), v_valid(false), w_valid(false)
+	{
+		
+	}
+
+	void set_r_gate_count(unsigned rcount)
+	{
+		r_gate_count = rcount;
+
+
+		r.resize(360, std::vector<double>(rcount, VALUE_INVALID));
+	}
+
+	void set_vw_gate_count(unsigned rcount)
+	{
+		vw_gate_count = rcount;
+		v.resize(360, std::vector<double>(rcount, VALUE_INVALID));
+		w.resize(360, std::vector<double>(rcount, VALUE_INVALID));
+	}
+
+	bool push_back(unsigned az, unsigned char* pdata)
+	{
+		if(az >= 360) az -= 360;
+
+		if(r_valid_this) 
+		{
+			push_back_r( az,  pdata);
+		}
+		if(v_valid_this) 
+		{
+			push_back_v( az, pdata);
+		}
+		if(w_valid_this) 
+		{
+			push_back_w( az, pdata);
+		}
+
+		return true;
+	}
+
+
+private:
+
+	//* 类型：实型，返回解码后的反射率（DBZ）。
+	double decode_ref(unsigned char code)
+	{
+		if(code==CODE_INVALID)		return VALUE_INVALID;
+		else if(code==CODE_RANFOLD)	return VALUE_RANFOLD;
+		else						
+			return (code-2.)/2.-32.5;
+	}
+
+	//* 类型：实型，返回解码后的径向速度（M/S）。
+	float decode_vel(unsigned char code, short ResType)
+	{
+		if(code==CODE_INVALID)		return VALUE_INVALID;
+		else if(code==CODE_RANFOLD)	return VALUE_RANFOLD;
+		else						
+		{
+			if(ResType==RES_POINT_FIVE) //0.5 m/s 
+				return (float((code-2.)/2.-63.5));
+			else
+				return (float((code-2)-127.));
+		}
+	}
+
+	//* 类型：实型，返回解码后的谱宽（M/S）。
+	float decode_spw(unsigned char code)
+	{
+		if(code==CODE_INVALID)		return VALUE_INVALID;
+		else if(code==CODE_RANFOLD)	return VALUE_RANFOLD;
+		else						
+			return (float((code-2.)/2.-63.5));
+	}
+	bool push_back_r(unsigned az, unsigned char* pdata)
+	{
+		//Get first bin, last bin, and number of bins
+		int FstBin = int(r_first_distance/r_gate_length+0.5);
+		int BinNum = r_gate_count;
+		if(FstBin<0)
+		{
+			BinNum = FstBin+BinNum;
+			FstBin = -1*FstBin;
+		}
+		int LstBin = FstBin + BinNum;
+
+		//Save data
+		for(int BnIndex=FstBin; BnIndex<LstBin; BnIndex++)
+		{
+			r[az][BnIndex] = decode_ref(pdata[r_pos+BnIndex]);
+		}
+
+		return true;
+	}
+	bool push_back_v(unsigned az, unsigned char* pdata)
+	{
+		//Get first bin, last bin, and number of bins
+		int FstBin = int(vw_first_distance/vw_gate_length+0.5);
+		int BinNum = vw_gate_count;
+		if(FstBin<0)
+		{
+			BinNum = FstBin+BinNum;
+			FstBin = -1*FstBin;
+		}
+		int LstBin = FstBin + BinNum;
+
+		//Save data
+		for(int BnIndex=FstBin; BnIndex<LstBin; BnIndex++)
+		{
+			v[az][BnIndex] = decode_vel(pdata[v_pos+BnIndex], v_resolution);
+		}
+		return true;
+	}
+	bool push_back_w(unsigned az, unsigned char* pdata)
+	{
+		//Get first bin, last bin, and number of bins
+		int FstBin = int(vw_first_distance/vw_gate_length+0.5);
+		int BinNum = vw_gate_count;
+		if(FstBin<0)
+		{
+			BinNum = FstBin+BinNum;
+			FstBin = -1*FstBin;
+		}
+		int LstBin = FstBin + BinNum;
+
+		//Save data
+		for(int BnIndex=FstBin; BnIndex<LstBin; BnIndex++)
+		{
+			w[az][BnIndex] = decode_spw(pdata[w_pos+BnIndex]);
+		}
+		return true;
+	}
+};
+
 class SA_SB_Info
 {
 public:
-	typedef RadarBaseData<2300> SA_Base,SB_Base;
+	typedef RadarBaseData<2300> SA_Base,SB_Base;//RGates+VGates+WGates
 	enum RADIAL_STATUS {
 		VOL_BEG=3,   //体扫开始状态标志
 		VOL_END=4,	 //体扫结束状态标志
@@ -102,8 +260,30 @@ public:
 	};
 
 public:
-	std::vector<SB_Base> alldata;//RGates+VGates+WGates
+	std::vector<SB_Base> alldata;
+
+	class compare_by_double
+	{
+	public:
+		bool operator() (const std::string &left, const std::string &right)const
+		{
+			double leftd, rightd;
+			std::istringstream instr(left+' '+right);
+			instr>>leftd>>rightd;
+
+			return leftd < rightd;
+		}
+	};
+	std::map<std::string, RadarElevation, compare_by_double> elevations;
+
 public:
+	SA_SB_Info()
+	{};
+	SA_SB_Info(const std::string& filename)
+	{
+		read_base_data(filename);
+	};
+
 	bool read_base_data(const std::string& filename)
 	{
 		//"Z_RADR_I_Z9398_20120717041200_O_DOR_SB_CAP.bin"
@@ -125,8 +305,17 @@ public:
 
 		fin.close();
 
+		init_elevations();//初始化各个仰角层 存储于map中
+
 		return true;
 	}
+
+	const RadarElevation & operator() (const std::string &el)const
+	{
+		return elevations.find(el)->second;
+	}
+
+	
 
 	void out_info(std::ostream &out)
 	{
@@ -189,6 +378,94 @@ public:
 	}
 
 private:
+
+	//将仰角转换为小数点后一位的字符串
+	std::string trans_to_el(double el)
+	{
+		std::ostringstream ostr;
+		ostr.precision(1);
+		ostr<<std::fixed<<el;
+
+		return ostr.str();
+	}
+	//转换为实际的仰角
+	double trans_angle(unsigned short angle)
+	{
+		return angle/8.0*180.0/4096.0;
+	}
+
+
+	
+	bool init_elevations()
+	{
+		std::string cur_el;
+		double angle;
+
+		//循环各个仰角的各个径向
+		for(std::vector<SB_Base>::iterator it=alldata.begin(); it != alldata.end(); it++)
+		{
+			switch(it->RadialStatus)
+			{
+			case VOL_BEG:
+
+			case ELV_BEG:  //仰角开始
+				angle = int(trans_angle(it->El)*10+0.5)/10.0;
+				cur_el = trans_to_el(angle);
+
+				elevations[cur_el].Elevation = angle;
+				
+				if(it->PtrOfReflectivity)
+				{
+					elevations[cur_el].set_r_gate_count(it->GatesNumberOfReflectivity);
+
+					elevations[cur_el].r_gate_length = it->GateSizeOfReflectivity;
+					elevations[cur_el].r_first_distance = it->RangeToFirstGateOfRef;
+
+					elevations[cur_el].r_valid  = true;
+					elevations[cur_el].r_pos = it->PtrOfReflectivity - 100;
+				}
+				if(it->PtrOfVelocity)
+				{
+					elevations[cur_el].set_vw_gate_count(it->GatesNumberOfDoppler);
+
+					elevations[cur_el].vw_gate_length = it->GateSizeOfDoppler;
+					elevations[cur_el].vw_first_distance = it->RangeToFirstGateOfDop;
+
+					elevations[cur_el].v_valid  = true;
+					elevations[cur_el].v_pos = it->PtrOfVelocity - 100;
+
+					elevations[cur_el].v_resolution = it->ResolutionOfVelocity;
+				}
+				if(it->PtrOfSpectrumWidth)
+				{
+					elevations[cur_el].w_valid  = true;
+					elevations[cur_el].w_pos = it->PtrOfSpectrumWidth - 100;
+				}
+
+				elevations[cur_el].r_valid_this  = it->PtrOfReflectivity;
+				elevations[cur_el].v_valid_this  = it->PtrOfVelocity;
+				elevations[cur_el].w_valid_this  = it->PtrOfSpectrumWidth;
+				
+				break;
+
+			case VOL_END:
+				  break;
+
+			case ELV_END:
+				  break;
+			case ELV_NORMAL:
+				break;
+			default:
+				;
+			}
+			//当前仰角层上加入本次方位角数据
+			elevations[cur_el].push_back(int(trans_angle(it->Az) +0.5), it->Echodata);
+		}
+
+		return true;
+	}
+
+	//获取径向状态
 	std::string get_radial_status(unsigned short RadialStatus)
 	{
 		std::string status;
@@ -211,7 +488,7 @@ private:
 
 		return status;
 	}
-
+	//获取体扫模式
 	std::string get_vol_model(unsigned short VcpNumber)
 	{
 		std::string status;
